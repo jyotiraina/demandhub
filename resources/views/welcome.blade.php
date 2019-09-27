8763195dcabfb6209f7c66c5752e14e6a64fcc94
ChinmayJoshi98 <joshichinmay94@gmail.com>
2019-09-27 15:17:50 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel correct vala abcd
                 </div>
 
                 <div class="links">
